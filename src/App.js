97da1555950c933895e9831ee249c8262ade41f9
SebastianUdacity <sebastianudacity7@gmail.com>
2020-07-29 23:07:45 +0100

    feat: Add NavBar routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
@@ -8,19 +8,22 @@ import MyQuestions from './components/MyQuestions'
 import NewQuestion from './components/NewQuestion'
 import LoginPage from './components/LoginPage'
 import PollPage from './components/PollPage'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <HomePage/>
-      <LeaderBoard/>
-      <MyQuestions/>
-      <NewQuestion/>
-      <LoginPage/>
-      <PollPage/>
+
+      <Route exact path="/" component={HomePage} /> 
+      <Route path="/leaderboard" component={LeaderBoard} /> 
+      <Route path="/myquestions" component={MyQuestions} /> 
+      <Route path="/new" component={NewQuestion} /> 
+      <Route path="/login" component={LoginPage} /> 
+      <Route path="/poll/:id" component={PollPage} /> 
+
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = () => {
-    
-    return (<div>NavBar</div>)
+
+    return (<div>
+        <Link to="/">Home</Link>
+        <span>|</span>
+        <Link to="/leaderboard">Leader Board</Link>
+        <span>|</span>
+        <Link to="/myquestions">My Questions</Link>
+        <span>|</span>
+        <Link to="/new">New Question</Link>
+    </div>)
 }
 
 
